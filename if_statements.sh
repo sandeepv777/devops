772c299dcb6f9b8123779150673b2e5a680d956f
sandeepv777 <66484921+sandeepv777@users.noreply.github.com>
2020-06-22 14:30:33 +0530

    if statement

--- if_statements.sh
+++ if_statements.sh
@@ -5,10 +5,10 @@ read n
 if [ $n -lt 10 ];
 then
 echo " it is a single digit number"
-fi
+
   if [ $n -lt 100 ];
 echo "it is a double digit number"
 else
   echo "it is a three digit number"
 fi
-
+fi
