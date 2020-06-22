40b7afaccf2fac7ae82071206e34f4942aa45fe7
sandeepv777 <66484921+sandeepv777@users.noreply.github.com>
2020-06-22 14:01:54 +0530

    if statement

--- if_statements.sh
+++ if_statements.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 n=10
-if [$n= - lt 10]
+if [ $n= -lt 10 ];
 then
-  echo"it is a single digit number"
-  else
-    echo" it is a double digit number"
-    fi
+echo "it is a single digit number"
+else
+echo " it is a double digit number"
+fi
